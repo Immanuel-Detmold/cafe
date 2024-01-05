d8c36dd34e4c8371f4289d6aa5023442cb857033
Ronny Schlidt <r.schlidt1@web.de>
2024-01-05 15:03:28 +0100

    chore: start shadcn

--- src/pages/Me.tsx
+++ src/pages/Me.tsx
@@ -1,4 +1,5 @@
 import { useUser } from '../data/useUser'
+import { Button } from '@/components/ui/button'
 
 export const Me = () => {
   const { user } = useUser()
@@ -6,6 +7,7 @@ export const Me = () => {
     <div>
       <h1>Me</h1>
       <pre>{JSON.stringify(user, undefined, 2)}</pre>
+      <Button>Click me</Button>
     </div>
   )
 }
