a2aefe74d42d4121c8d9633aa1c1fcfaeff7def6
Kai Wittmann <kai@heart-co.de>
2024-01-05 15:17:18 +0100

    fix: make darkmode working

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { router } from './router'
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
     </ThemeProvider>
   )
