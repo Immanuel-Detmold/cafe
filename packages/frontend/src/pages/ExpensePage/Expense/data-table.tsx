8262686df6ddf91010a7a041673f969889784252
Ronny Schlidt <r.schlidt1@web.de>
2024-08-08 21:09:23 +0200

    chore:update

--- packages/frontend/src/pages/ExpensePage/Expense/data-table.tsx
+++ packages/frontend/src/pages/ExpensePage/Expense/data-table.tsx
@@ -45,6 +45,7 @@ export function DataTable<TData, TValue>({
   // dataDates,
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = React.useState<SortingState>([])
+
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     [],
   )
